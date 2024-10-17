e6ee3465ed4f9924cd3cf6d8f249ae7cc722d576
BBII1 <66112746@dpu.ac.th>
2024-10-17 16:38:58 +0700

    first commit

--- 67112347/heha.py
+++
@@ -1,7 +0,0 @@
-task = ["clean the house", "shopping"]
-def add_task(task,new_tasks):
-    task.append(new_tasks)
-    return task
-
-print("เพิ่ม 'math homework' ในลิสต์ task :",add_task(task,"math homework"))
-
